786cfbaa35db6861dddb33d349792dd9540632bb
strategist k <815034557@qq.com>
2016-11-20 19:44:22 +0800

    Thread
    异步消息处理的使用方法

--- app/src/main/java/com/study/android/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/study/android/androidthreadtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.study.android.androidthreadtest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +9,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final int UPDATE_TEXT = 1;
+
     private TextView text;
     private Button changeText;
+
+    private Handler mHandler = new Handler(){
+        public void handleMessage (Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("你好  世界！！！！");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        text.setText("nice to meet you");
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        mHandler.sendMessage(message);
                     }
                 }).start();
                 break;
